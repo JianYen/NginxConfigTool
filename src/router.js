882a25bd2c0f422020dae9611c6b684ee46c2011
wesley <tgfc040@tgfc.tw>
2019-08-21 13:24:16 +0800

    init vue & bootstrap & create page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login'
+import About from './views/About'
+import Home from './views/Home'
 
 Vue.use(Router)
 
@@ -10,16 +12,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/about',
+      name:'about',
+      component:About
+    },
+    {
+      path:'/home',
+      name:'home',
+      component:Home
     }
+
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <b-table
+                id="my-table"
+                :items="items"
+                :per-page="perPage"
+                :current-page="currentPage"
+                small
+        ></b-table>
+        <b-pagination
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="perPage"
+                first-text="First"
+                prev-text="Prev"
+                next-text="Next"
+                last-text="Last"
+        ></b-pagination>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            perPage: 3,
+            currentPage: 1,
+            items: [
+                { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+                { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+                { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+                { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+                { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+                { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+                { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+                { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+                ,
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' },
+                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+            ]
+        }
+    },
+    computed: {
+        rows() {
+            return 24
+        }
+    },
+    methods:{
+        changePage (number) {
+            this.pageListRequest.number = number - 1
+        },
+        changePageSize (pageSize) {
+            this.pageListRequest.pageSize = pageSize
+        },
+    }
+}
+</script>
